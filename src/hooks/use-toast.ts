b0421d8f1287cdcadc0a1dea5631e39b8f6d3cfd
Ujjwal Sinha <ujjwalsinha418@gmail.com>
2024-12-19 20:49:39 +0530

    db connected

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -152,7 +152,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
+      onOpenChange: (open: any) => {
         if (!open) dismiss();
       },
     },
